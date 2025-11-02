94cc50ac9d9a16ec65678567c13277ed5d7524b1
Gabrielly Ferreira <155073067+gabs-fe@users.noreply.github.com>
2025-11-02 19:57:48 -0300

    fix: ajeita serialivers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,7 @@ from .models import TipoResiduo, PontoColeta, Cooperativa
 class TipoResiduoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoResiduo
-        fields = ['id', 'nome', 'descricao', 'eh_reciclavel']
+        fields = ['id', 'nome', 'descricao', 'reciclavel']
 
 class PontoColetaSerializer(GeoFeatureModelSerializer):
    tipos_residuos_aceitos = serializers.StringRelatedField(many=True)
