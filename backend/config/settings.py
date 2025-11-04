413e938a82a230409db9ffbf2fdc81652f1a8fb0
Gabrielly Ferreira <155073067+gabs-fe@users.noreply.github.com>
2025-11-04 10:18:18 -0300

    fix: erro em cors headers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:8081" 
+    "http://localhost:8081",
     "http://127.0.0.1:3000",
 ]
 
